47cafa90992c3231e1f6c70931389420df18f5c4
Diego Martínez <a353198@uach.mx>
2024-05-24 00:06:42 -0600

    + secret & BD Variables

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+// config.php
+$secret_key = "cloudcomputing";
+$servername = "localhost"; // Puedes cambiar esto si tu servidor MySQL está en otro lugar
+$username = "root"; // Nombre de usuario de la base de datos
+$password = ""; // Contraseña de la base de datos
+$dbname = "proyjd"; // Nombre de la base de datos
+?>
